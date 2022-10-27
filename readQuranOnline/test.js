76522ab685adb0196fcb48c81fc1ffb28faf5cfd
Mohammed Yahia Hashem <myahia2000@hotmail.com>
2022-10-27 15:33:51 +0200

    Delete readQuranOnline directory

--- readQuranOnline/test.js
+++
@@ -1,9 +0,0 @@
-
-function test(txt){
-	for(i=1;i<=ayat;i++){
-		var col=document.getElementById("a"+sorah+"_"+i);
-		col.style.color="#FFFFFF";
-	}
-	var col=document.getElementById(txt);
-	col.style.color="#FF0000";
-}
